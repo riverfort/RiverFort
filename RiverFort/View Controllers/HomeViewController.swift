5caa579a71796081c384da58f2931ffcd7b6c11c
Qiuyang Nie <qyang.nie@gmail.com>
2021-07-26 15:05:51 +0100

    Comment old search

--- RiverFort/View Controllers/HomeViewController.swift
+++ RiverFort/View Controllers/HomeViewController.swift
@@ -12,20 +12,20 @@ class HomeViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let searchVC = UINavigationController(rootViewController: SearchViewController())
+//        let searchVC = UINavigationController(rootViewController: SearchViewController())
         let newSearchVC = UINavigationController(rootViewController: NewSearchViewController())
         let watchlistVC = UINavigationController(rootViewController: WatchlistViewController())
         
-        searchVC.title = "Search"
+//        searchVC.title = "Search"
         newSearchVC.title = "Search"
         watchlistVC.title = "Watchlist"
 
-        self.setViewControllers([searchVC, newSearchVC, watchlistVC], animated: true)
+        self.setViewControllers([newSearchVC, watchlistVC], animated: true)
         guard let items = self.tabBar.items else {
             return
         }
         
-        let images = ["magnifyingglass.circle.fill", "magnifyingglass.circle.fill", "eye.circle.fill",]
+        let images = ["magnifyingglass.circle.fill", "eye.circle.fill",]
         
         for x in 0..<items.count {
             items[x].image = UIImage(systemName: images[x])
