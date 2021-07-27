4b266c3e7b6a3cf1c40f40c7bbb3a1dfb44ee548
Qiuyang Nie <qyang.nie@gmail.com>
2021-07-27 09:41:53 +0100

    Remove clearsSelectionOnViewWillAppear

--- RiverFort/Search/SearchResultsTableViewController.swift
+++ RiverFort/Search/SearchResultsTableViewController.swift
@@ -13,7 +13,6 @@ class SearchResultsTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.clearsSelectionOnViewWillAppear = false
         configureTableView()
     }
 }
