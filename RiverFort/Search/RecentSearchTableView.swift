f0f706d2853ad66f836a2c53fae27d7906b640ea
Qiuyang Nie <qyang.nie@gmail.com>
2021-07-28 09:03:51 +0100

    Add header section

--- RiverFort/Search/RecentSearchTableView.swift
+++ RiverFort/Search/RecentSearchTableView.swift
@@ -26,7 +26,7 @@ extension RecentSearchTableView {
     }
 }
 
-extension RecentSearchTableView: UITableViewDataSource, UITableViewDelegate {
+extension RecentSearchTableView: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 5
     }
@@ -35,5 +35,64 @@ extension RecentSearchTableView: UITableViewDataSource, UITableViewDelegate {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = "hello world"
         return cell
+    }
+}
+
+extension RecentSearchTableView: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        print("\(indexPath.row)")
+    }
+}
+
+extension RecentSearchTableView {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        let recentlySearchedLabel = UILabel()
+        let clearButton = UIButton(type: .system)
+        let hStack = UIStackView(arrangedSubviews: [recentlySearchedLabel, clearButton])
+        configureHeaderView(headerView: headerView)
+        configureRecentlySearchedLabel(recentlySearchedLabel: recentlySearchedLabel)
+        configureClearButton(clearButton: clearButton)
+        configureHeaderHStack(headerView: headerView, stackView: hStack)
+        return headerView
+    }
+}
+
+extension RecentSearchTableView {
+    private func configureHeaderView(headerView: UIView) {
+
+    }
+    
+    private func configureRecentlySearchedLabel(recentlySearchedLabel: UILabel) {
+        recentlySearchedLabel.text = "Recently Searched"
+        recentlySearchedLabel.font = .preferredFont(forTextStyle: .headline)
+        recentlySearchedLabel.adjustsFontForContentSizeCategory = true
+    }
+    
+    private func configureClearButton(clearButton: UIButton) {
+        clearButton.setTitleColor(.systemIndigo, for: .normal)
+        clearButton.setTitle("Clear", for: .normal)
+        clearButton.titleLabel!.font = .preferredFont(forTextStyle: .headline)
+        clearButton.titleLabel!.adjustsFontForContentSizeCategory = true
+    }
+    
+    private func configureHeaderHStack(headerView: UIView, stackView: UIStackView) {
+        headerView.addSubview(stackView)
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.leadingAnchor.constraint(equalTo: headerView.safeAreaLayoutGuide.leadingAnchor, constant: 12).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: headerView.safeAreaLayoutGuide.trailingAnchor, constant: -12).isActive = true
+        stackView.topAnchor.constraint(equalTo: headerView.safeAreaLayoutGuide.topAnchor, constant: 12).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: headerView.safeAreaLayoutGuide.centerYAnchor).isActive = true
     }
 }
