801f87566d05d2ddde955e7bf05319c88ad70f2f
Qiuyang Nie <qyang.nie@gmail.com>
2021-08-03 09:30:54 +0100

    Scaffold WatchlistTableViewCell

---
+++ RiverFort/Watchlist/View/WatchlistTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  WatchlistTableViewCell.swift
+//  RiverFort
+//
+//  Created by Qiuyang Nie on 03/08/2021.
+//
+
+import UIKit
+
+class WatchlistTableViewCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
