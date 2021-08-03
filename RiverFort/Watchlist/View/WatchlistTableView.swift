e0e7f81f015ef09d5e3d783c946b80e49df2d14d
Qiuyang Nie <qyang.nie@gmail.com>
2021-08-03 09:00:06 +0100

    Scaffold WatchlistTableView

---
+++ RiverFort/Watchlist/View/WatchlistTableView.swift
@@ -0,0 +1,40 @@
+//
+//  WatchlistTableView.swift
+//  RiverFort
+//
+//  Created by Qiuyang Nie on 03/08/2021.
+//
+
+import Foundation
+
+class WatchlistTableView: UITableView {
+    override init(frame: CGRect, style: UITableView.Style) {
+        super.init(frame: frame, style: style)
+        configureTableView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension WatchlistTableView {
+    private func configureTableView() {
+        self.backgroundColor = .systemBackground
+        self.dataSource = self
+        self.delegate = self
+        self.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+}
+
+extension WatchlistTableView: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
+}
--- RiverFort/Watchlist/ViewController/NewWatchlistViewController.swift
+++ RiverFort/Watchlist/ViewController/NewWatchlistViewController.swift
@@ -8,11 +8,18 @@
 import UIKit
 
 class NewWatchlistViewController: UIViewController {
-
+    private let watchlistTableView = WatchlistTableView(frame: .zero, style: .plain)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureView()
         configureNavigationController()
+        configureWatchlistTableView()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        setWatchlistTableViewConstraints()
     }
 }
 
@@ -26,5 +33,17 @@ extension NewWatchlistViewController {
         navigationItem.largeTitleDisplayMode = .always
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationBar.tintColor = .systemIndigo
+    }
+    
+    private func configureWatchlistTableView() {
+        view.addSubview(watchlistTableView)
+    }
+    
+    private func setWatchlistTableViewConstraints() {
+        watchlistTableView.translatesAutoresizingMaskIntoConstraints = false
+        watchlistTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive   = true
+        watchlistTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        watchlistTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive           = true
+        watchlistTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive     = true
     }
 }
