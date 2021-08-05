07af0e17bcefb1c43b4965d35a356a6a52f78154
Qiuyang Nie <qyang.nie@gmail.com>
2021-08-05 16:10:26 +0100

    Scaffold settings group

---
+++ RiverFort/Settings/ViewController/NewSettingsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  NewSettingsViewController.swift
+//  RiverFort
+//
+//  Created by Qiuyang Nie on 05/08/2021.
+//
+
+import UIKit
+
+class NewSettingsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
