4881cec0f8b085a019ad5f2f0b6cc49ea9c4b9f5
Qiuyang Nie <qyang.nie@gmail.com>
2021-08-06 08:14:53 +0100

    Add configView and configNavigationController

--- RiverFort/Settings/ViewController/NewSettingsViewController.swift
+++ RiverFort/Settings/ViewController/NewSettingsViewController.swift
@@ -10,5 +10,20 @@ import UIKit
 class NewSettingsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        configView()
+        configNavigationController()
+    }
+}
+
+extension NewSettingsViewController {
+    private func configView() {
+        view.backgroundColor = .systemBackground
+    }
+    
+    private func configNavigationController() {
+        navigationItem.title = "Settings"
+        navigationItem.largeTitleDisplayMode = .always
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.tintColor = .systemIndigo
     }
 }
