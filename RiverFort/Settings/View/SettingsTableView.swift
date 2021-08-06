5fee88721746258989024ddb8c2521938f347943
Qiuyang Nie <qyang.nie@gmail.com>
2021-08-06 08:22:26 +0100

    Scaffold SettingsTableView

--- RiverFort/Settings/View/SettingsTableView.swift
+++ RiverFort/Settings/View/SettingsTableView.swift
@@ -10,9 +10,31 @@ import UIKit
 class SettingsTableView: UITableView {
     override init(frame: CGRect, style: UITableView.Style) {
         super.init(frame: frame, style: style)
+        configTableView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension SettingsTableView {
+    private func configTableView() {
+        self.backgroundColor = .systemBackground
+        self.dataSource = self
+        self.delegate = self
+        self.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+}
+
+extension SettingsTableView: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hello"
+        return cell
     }
 }
--- RiverFort/Settings/ViewController/NewSettingsViewController.swift
+++ RiverFort/Settings/ViewController/NewSettingsViewController.swift
@@ -8,10 +8,18 @@
 import UIKit
 
 class NewSettingsViewController: UIViewController {
+    private let settingsTableView = SettingsTableView(frame: .zero, style: .plain)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configView()
         configNavigationController()
+        configSettingsTableView()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        setSettingsTableViewConstraints()
     }
 }
 
@@ -25,5 +33,17 @@ extension NewSettingsViewController {
         navigationItem.largeTitleDisplayMode = .always
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationBar.tintColor = .systemIndigo
+    }
+    
+    private func configSettingsTableView() {
+        view.addSubview(settingsTableView)
+    }
+    
+    private func setSettingsTableViewConstraints() {
+        settingsTableView.translatesAutoresizingMaskIntoConstraints = false
+        settingsTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive   = true
+        settingsTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        settingsTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive     = true
+        settingsTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive           = true
     }
 }
