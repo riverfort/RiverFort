72b43adf9d7e3cf373618b5dca84abd4d445d471
Qiuyang Nie <qyang.nie@gmail.com>
2021-08-06 08:34:12 +0100

    Scaffold SettingsConstant

---
+++ RiverFort/Settings/Constant/SettingsConstant.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsConstant.swift
+//  RiverFort
+//
+//  Created by Qiuyang Nie on 06/08/2021.
+//
+
+import Foundation
+
+class SettingsConstant {
+    static var TODO_1 = "com.riverfort."
+    static var TODO_2 = "com.riverfort."
+}
