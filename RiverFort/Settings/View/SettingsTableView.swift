ae37e600ef1488307bdbf49fa33e4b69a95fcd72
Qiuyang Nie <qyang.nie@gmail.com>
2021-08-06 08:16:40 +0100

    Add SettingsTableView

---
+++ RiverFort/Settings/View/SettingsTableView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsTableView.swift
+//  RiverFort
+//
+//  Created by Qiuyang Nie on 06/08/2021.
+//
+
+import UIKit
+
+class SettingsTableView: UITableView {
+    override init(frame: CGRect, style: UITableView.Style) {
+        super.init(frame: frame, style: style)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
