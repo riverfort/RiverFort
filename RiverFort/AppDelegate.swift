f0526731ec6e2b6d2e628e8d518f2021de36dda7
Qiuyang Nie <qyang.nie@gmail.com>
2021-08-15 11:57:23 +0100

    Commented out isFirstLaunch

--- RiverFort/AppDelegate.swift
+++ RiverFort/AppDelegate.swift
@@ -15,11 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        if UIApplication.isFirstLaunch() {
-            WatchlistSync.prepareRegisterUserDevice()
-        } else {
-            print("Not First Run")
-        }
+//        if UIApplication.isFirstLaunch() {
+//            WatchlistSync.prepareRegisterUserDevice()
+//        } else {
+//            print("Not First Run")
+//        }
 
         return true
     }
