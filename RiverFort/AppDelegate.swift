1ece674be07e85e696b60a8c024102179afc0b6e
Qiuyang Nie <qyang.nie@gmail.com>
2021-08-15 12:21:44 +0100

    Registering With APNs

--- RiverFort/AppDelegate.swift
+++ RiverFort/AppDelegate.swift
@@ -96,6 +96,28 @@ extension AppDelegate {
     func getNotificationSettings() {
       UNUserNotificationCenter.current().getNotificationSettings { settings in
         print("Notification settings: \(settings)")
+        guard settings.authorizationStatus == .authorized else { return }
+        DispatchQueue.main.async {
+          UIApplication.shared.registerForRemoteNotifications()
+        }
       }
+    }
+}
+
+extension AppDelegate {
+    func application(
+      _ application: UIApplication,
+      didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+    ) {
+      let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
+      let token = tokenParts.joined()
+      print("Device Token: \(token)")
+    }
+    
+    func application(
+      _ application: UIApplication,
+      didFailToRegisterForRemoteNotificationsWithError error: Error
+    ) {
+      print("Failed to register: \(error)")
     }
 }
