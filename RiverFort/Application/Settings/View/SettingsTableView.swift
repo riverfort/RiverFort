c3c24f129493b48bd7a34120db3a05f2823a5a77
Qiuyang Nie <qyang.nie@gmail.com>
2021-08-24 12:21:32 +0100

    Format

--- RiverFort/Application/Settings/View/SettingsTableView.swift
+++ RiverFort/Application/Settings/View/SettingsTableView.swift
@@ -65,13 +65,15 @@ extension SettingsTableView: UITableViewDataSource, UITableViewDelegate {
         let settingsOptionType = settingsSections[indexPath.section].options[indexPath.row]
         switch settingsOptionType.self {
         case .staticCell(newSettingsStaticOption: let newSettingsStaticOption):
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingsTableViewConstant.STATIC_TABLE_VIEW_CELL,
-                                                           for: indexPath) as? SettingsStaticTableViewCell else { return UITableViewCell() }
+            guard let cell = tableView.dequeueReusableCell(
+                    withIdentifier: SettingsTableViewConstant.STATIC_TABLE_VIEW_CELL,
+                    for: indexPath) as? SettingsStaticTableViewCell else { return UITableViewCell() }
             cell.setSettingsStaticTableViewCell(newSettingsStaticOption: newSettingsStaticOption)
             return cell
         case .switchCell(newSettingsSwitchOption: let newSettingsSwitchOption):
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingsTableViewConstant.SWITCH_TABLE_VIEW_CELL,
-                                                           for: indexPath) as? SettingsSwitchTableViewCell else { return UITableViewCell() }
+            guard let cell = tableView.dequeueReusableCell(
+                    withIdentifier: SettingsTableViewConstant.SWITCH_TABLE_VIEW_CELL,
+                    for: indexPath) as? SettingsSwitchTableViewCell else { return UITableViewCell() }
             cell.setSettingsSwitchTableViewCell(newSettingsSwitchOption: newSettingsSwitchOption)
             cell.selectionStyle = .none
             return cell
