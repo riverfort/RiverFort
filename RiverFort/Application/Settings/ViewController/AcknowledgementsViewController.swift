b9b3d44883c93b5aaaed587dc152ac03b920ca69
Qiuyang Nie <qyang.nie@gmail.com>
2021-08-25 13:52:46 +0100

    Remove addHyperLinksToText

--- RiverFort/Application/Settings/ViewController/AcknowledgementsViewController.swift
+++ RiverFort/Application/Settings/ViewController/AcknowledgementsViewController.swift
@@ -35,8 +35,8 @@ extension AcknowledgementsViewController {
         textView.isEditable = false
         textView.font = .preferredFont(forTextStyle: .callout)
         textView.adjustsFontForContentSizeCategory = true
-        textView.text = "Data provided by Financial Modeling Prep"
-        textView.addHyperLinksToText(originalText: "Data provided by Financial Modeling Prep", hyperLinks: ["Financial Modeling Prep": "https://financialmodelingprep.com/developer/docs/"])
+        textView.dataDetectorTypes = UIDataDetectorTypes.link
+        textView.text = "Data provided by Financial Modeling Prep: https://financialmodelingprep.com/developer/docs/"
     }
     
     private func setTextViewConstraints() {
@@ -46,25 +46,4 @@ extension AcknowledgementsViewController {
         textView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10).isActive     = true
         textView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive            = true
     }
-}
-
-extension UITextView {
-  func addHyperLinksToText(originalText: String, hyperLinks: [String: String]) {
-    let style = NSMutableParagraphStyle()
-    style.alignment = .left
-    let attributedOriginalText = NSMutableAttributedString(string: originalText)
-    for (hyperLink, urlString) in hyperLinks {
-        let linkRange = attributedOriginalText.mutableString.range(of: hyperLink)
-        let fullRange = NSRange(location: 0, length: attributedOriginalText.length)
-        attributedOriginalText.addAttribute(NSAttributedString.Key.link, value: urlString, range: linkRange)
-        attributedOriginalText.addAttribute(NSAttributedString.Key.paragraphStyle, value: style, range: fullRange)
-        attributedOriginalText.addAttribute(NSAttributedString.Key.font, value: UIFont.preferredFont(forTextStyle: .callout), range: fullRange)
-    }
-
-    self.linkTextAttributes = [
-        NSAttributedString.Key.foregroundColor: UIColor.link,
-        NSAttributedString.Key.underlineStyle: NSUnderlineStyle.single.rawValue,
-    ]
-    self.attributedText = attributedOriginalText
-  }
 }
