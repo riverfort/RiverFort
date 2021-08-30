5e7211f960f76efd4c4e1b7729af20b336e1fbdf
Qiuyang Nie <qyang.nie@gmail.com>
2021-08-30 11:39:43 +0100

    Scaffold Search v2

--- RiverFort/Application/Main/HomeViewController.swift
+++ RiverFort/Application/Main/HomeViewController.swift
@@ -10,20 +10,22 @@ import UIKit
 class HomeViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        let searchV2VC = UINavigationController(rootViewController: SearchV2ViewController())
         let searchVC = UINavigationController(rootViewController: SearchViewController())
         let newWatchlistVC = UINavigationController(rootViewController: WatchlistViewController())
         let settingsVC = UINavigationController(rootViewController: SettingsViewController())
         
+        searchV2VC.title = "Search"
         searchVC.title = "Search"
         newWatchlistVC.title = "Watchlist"
         settingsVC.title = "Settings"
 
-        self.setViewControllers([searchVC, newWatchlistVC, settingsVC], animated: true)
+        self.setViewControllers([searchV2VC, searchVC, newWatchlistVC, settingsVC], animated: true)
         guard let items = self.tabBar.items else {
             return
         }
         
-        let images = ["magnifyingglass.circle.fill", "heart.text.square.fill", "gear"]
+        let images = ["magnifyingglass.circle.fill", "magnifyingglass.circle.fill", "heart.text.square.fill", "gear"]
         
         let configuration = UIImage.SymbolConfiguration(weight: .heavy)
         for x in 0..<items.count {
