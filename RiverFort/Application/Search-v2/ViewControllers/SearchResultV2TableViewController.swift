6482a493d3af283e425b3de5c65b351fdd2f549b
Qiuyang Nie <qyang.nie@gmail.com>
2021-08-30 14:48:26 +0100

    Add search

---
+++ RiverFort/Application/Search-v2/ViewControllers/SearchResultV2TableViewController.swift
@@ -0,0 +1,52 @@
+//
+//  SearchResultV2TableViewController.swift
+//  RiverFort
+//
+//  Created by Qiuyang Nie on 30/08/2021.
+//
+
+import UIKit
+
+class SearchResultV2TableViewController: UITableViewController {
+    private var companies: [YahooFinanceSearchedCompany] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configTableView()
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+    }
+}
+
+extension SearchResultV2TableViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return companies.count
+    }
+}
+
+extension SearchResultV2TableViewController {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! SearchV2ResultCell
+        let company = companies[indexPath.row]
+        cell.setCell(for: company)
+        return cell
+    }
+}
+
+extension SearchResultV2TableViewController {
+    private func configTableView() {
+        tableView.register(SearchV2ResultCell.self, forCellReuseIdentifier: "cell")
+        tableView.estimatedRowHeight = 85.0
+        tableView.rowHeight = UITableView.automaticDimension
+    }
+}
+
+extension SearchResultV2TableViewController {
+    public func setCompanies(companies: [YahooFinanceSearchedCompany]) {
+        self.companies = companies
+    }
+}
--- RiverFort/Application/Search-v2/ViewControllers/SearchV2ViewController.swift
+++ RiverFort/Application/Search-v2/ViewControllers/SearchV2ViewController.swift
@@ -6,24 +6,83 @@
 //
 
 import UIKit
+import Alamofire
 
 class SearchV2ViewController: UIViewController {
+    private let searchResultV2TableViewController = SearchResultV2TableViewController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configView()
+        configNavigationController()
+        configSearchController()
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension SearchV2ViewController {
+    private func configView() {
+        view.backgroundColor = .systemBackground
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configNavigationController() {
+        navigationItem.title = "Search"
+        navigationItem.largeTitleDisplayMode = .always
+        navigationItem.hidesSearchBarWhenScrolling = false
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.tintColor = .systemIndigo
     }
-    */
+    
+    private func configSearchController() {
+        
+        let searchController = UISearchController(searchResultsController: searchResultV2TableViewController)
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        searchController.searchBar.placeholder = "Symbols, Companies"
+        navigationItem.searchController = searchController
+    }
+}
 
+extension SearchV2ViewController: UISearchBarDelegate {
+    
+}
+
+extension SearchV2ViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let searchText = searchController.searchBar.text else {
+            return
+        }
+        yahooFinanceSearch(for: searchText)
+    }
+}
+
+extension SearchV2ViewController {
+    private func yahooFinanceSearch(for searchTerm: String) {
+        guard !searchTerm.isEmpty else {
+            return
+        }
+        AF.request("https://finance.yahoo.com/_finance_doubledown/api/resource/searchassist;searchTerm=\(searchTerm)")
+            .validate()
+            .responseDecodable(of: YahooFinanceSearchedResult.self) { [self] response in
+                guard let yahooFinanceSearch = response.value else { return }
+                searchResultV2TableViewController.setCompanies(companies: yahooFinanceSearch.items)
+                searchResultV2TableViewController.tableView.reloadData()
+            }
+    }
+}
+
+struct YahooFinanceSearchedCompany: Decodable {
+    let symbol: String
+    let name: String
+    let exch: String
+    let type: String
+    let exchDisp: String
+    let typeDisp: String
+}
+
+struct YahooFinanceSearchedResult: Decodable {
+    let suggestionTitleAccessor: String
+    let suggestionMeta: [String]
+    let hiConf: Bool
+    let items: [YahooFinanceSearchedCompany]
 }
