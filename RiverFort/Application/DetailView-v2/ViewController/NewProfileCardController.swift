9a724aa006b35ac3cf0b3a39280c18a1415da2c0
Qiuyang Nie <qyang.nie@gmail.com>
2021-09-01 11:56:10 +0100

    Add NewProfileCardController

---
+++ RiverFort/Application/DetailView-v2/ViewController/NewProfileCardController.swift
@@ -0,0 +1,15 @@
+//
+//  NewProfileCardController.swift
+//  RiverFort
+//
+//  Created by Qiuyang Nie on 01/09/2021.
+//
+
+import UIKit
+import CardParts
+
+class NewProfileCardController: CardPartsViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
