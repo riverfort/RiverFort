2db213ec9b949ab409632cc7d300658237b0f415
Qiuyang Nie <qyang.nie@gmail.com>
2021-09-02 11:01:54 +0100

    Add fetchUKNewsfeed

--- RiverFort/Application/DetailView-v2/Model/DetailViewAPIFunction.swift
+++ RiverFort/Application/DetailView-v2/Model/DetailViewAPIFunction.swift
@@ -9,7 +9,11 @@ import Foundation
 import Alamofire
 
 class DetailViewAPIFunction {
-    static func fetchHistoricalPriceFromRiverFort(symbol: String) {
+    static func fetchHistoricalPriceFromRiverFort(symbol: String) -> DataRequest {
         AF.request("https://data.riverfort.com/api/v1/companies/\(symbol)/trading?ordering=market_date").validate()
+    }
+    
+    static func fetchUKNewsfeed(symbol: String) -> DataRequest {
+        AF.request("https://www.investegate.co.uk/Rss.aspx?company=\(symbol)").validate()
     }
 }
