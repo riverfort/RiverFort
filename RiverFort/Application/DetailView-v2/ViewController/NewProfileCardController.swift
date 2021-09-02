5a4dfcf7030711c49c804a9e53eef89d8bd8cd5c
Qiuyang Nie <qyang.nie@gmail.com>
2021-09-02 09:24:58 +0100

    Add NewProfileCardController

--- RiverFort/Application/DetailView-v2/ViewController/NewProfileCardController.swift
+++ RiverFort/Application/DetailView-v2/ViewController/NewProfileCardController.swift
@@ -9,12 +9,22 @@ import UIKit
 import CardParts
 
 class NewProfileCardController: CardPartsViewController {
-    private var namePart = CardPartTitleView(type: .titleOnly)
+    private let namePart = CardPartTitleView(type: .titleOnly)
+    private let exchPart = CardPartTitleView(type: .titleOnly)
+    private let pricePart  = CardPartTitleView(type: .titleOnly)
+    private let changePart = CardPartTitleView(type: .titleOnly)
+    private let cardPartSV1 = CardPartStackView()
+    private let cardPartSV2 = CardPartStackView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configView()
         configNamePart()
+        configExchPart()
+        configPricePart()
+        configChangePart()
+        configCardPartSV1()
+        configCardPartSV2()
         configCardParts()
     }
 }
@@ -31,11 +41,47 @@ extension NewProfileCardController {
         namePart.titleColor = .label
         namePart.titleFont = .preferredFont(forTextStyle: .headline)
         namePart.label.adjustsFontForContentSizeCategory = true
-        namePart.margins.left = 15
+    }
+    
+    private func configExchPart() {
+        exchPart.title = "Exch"
+        exchPart.titleColor = .label
+        exchPart.titleFont = .preferredFont(forTextStyle: .subheadline)
+        exchPart.label.adjustsFontForContentSizeCategory = true
+    }
+    
+    private func configPricePart() {
+        pricePart.title = "122.33"
+        pricePart.titleColor = .label
+        pricePart.titleFont = .preferredFont(forTextStyle: .headline)
+        pricePart.label.adjustsFontForContentSizeCategory = true
+    }
+    
+    private func configChangePart() {
+        changePart.title = "+10.0%"
+        changePart.titleColor = .label
+        changePart.titleFont = .preferredFont(forTextStyle: .subheadline)
+        changePart.label.adjustsFontForContentSizeCategory = true
+    }
+    
+    private func configCardPartSV1() {
+        cardPartSV1.axis = .horizontal
+        cardPartSV1.distribution = .equalSpacing
+        cardPartSV1.addArrangedSubview(namePart)
+        cardPartSV1.addArrangedSubview(exchPart)
+    }
+    
+    private func configCardPartSV2() {
+        cardPartSV2.axis = .vertical
+        cardPartSV2.spacing = 5
+        cardPartSV2.distribution = .equalSpacing
+        cardPartSV2.margins = UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15)
+        cardPartSV2.addArrangedSubview(cardPartSV1)
+        cardPartSV2.addArrangedSubview(pricePart)
     }
     
     private func configCardParts() {
-        setupCardParts([namePart])
+        setupCardParts([cardPartSV2])
     }
 }
 
