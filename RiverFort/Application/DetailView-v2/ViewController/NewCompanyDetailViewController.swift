39ab43b86c86b1a79d39431de79aef42d5fc2cca
Qiuyang Nie <qyang.nie@gmail.com>
2021-09-02 16:54:20 +0100

    Add OHLCCardController

--- RiverFort/Application/DetailView-v2/ViewController/NewCompanyDetailViewController.swift
+++ RiverFort/Application/DetailView-v2/ViewController/NewCompanyDetailViewController.swift
@@ -10,7 +10,7 @@ import CardParts
 
 class NewCompanyDetailViewController: CardsViewController {
     private var symbol: String = ""
-    private let cards: [CardController] = [NewProfileCardController(), NewPriceChartCardController(), NewADTVChartCardController(), NewADTVCardController()]
+    private let cards: [CardController] = [NewProfileCardController(), NewPriceChartCardController(), NewADTVChartCardController(), NewADTVCardController(), OHLCCardController()]
     
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ RiverFort/Application/DetailView-v2/ViewController/OHLCCardController.swift
@@ -0,0 +1,123 @@
+//
+//  OHLCCardController.swift
+//  RiverFort
+//
+//  Created by Qiuyang Nie on 02/09/2021.
+//
+
+import UIKit
+import CardParts
+
+class OHLCCardController: TemplateCardController {
+    private let titlePart = CardPartTitleView(type: .titleOnly)
+    
+    private let openLabelPart = CardPartTitleView(type: .titleOnly)
+    private let openDataPart = CardPartTitleView(type: .titleOnly)
+    private let openCardPartSV = CardPartStackView()
+    
+    private let highLabelPart = CardPartTitleView(type: .titleOnly)
+    private let highDataPart = CardPartTitleView(type: .titleOnly)
+    private let highCardPartSV = CardPartStackView()
+    
+    private let lowLabelPart = CardPartTitleView(type: .titleOnly)
+    private let lowDataPart = CardPartTitleView(type: .titleOnly)
+    private let lowCardPartSV = CardPartStackView()
+    
+    private let closeLabelPart = CardPartTitleView(type: .titleOnly)
+    private let closeDataPart = CardPartTitleView(type: .titleOnly)
+    private let closeCardPartSV = CardPartStackView()
+    
+    private let cardPartSV = CardPartStackView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configTitlePart()
+        configADTVPart()
+        configCardPartSV()
+        setupCardParts([cardPartSV])
+    }
+}
+
+extension OHLCCardController {
+    private func configTitlePart() {
+        titlePart.title = "OHLC"
+        titlePart.label.numberOfLines = 0
+        titlePart.titleColor = .label
+        titlePart.titleFont = .preferredFont(forTextStyle: .headline)
+        titlePart.label.adjustsFontForContentSizeCategory = true
+    }
+    
+    private func configADTVPart() {
+        openLabelPart.title = "O"
+        openLabelPart.titleColor = .systemGray
+        openLabelPart.titleFont = .preferredFont(forTextStyle: .subheadline)
+        openLabelPart.label.adjustsFontForContentSizeCategory = true
+        
+        openDataPart.title = "100,000,000"
+        openDataPart.titleColor = .label
+        openDataPart.titleFont = .preferredFont(forTextStyle: .subheadline)
+        openDataPart.label.adjustsFontForContentSizeCategory = true
+        
+        openCardPartSV.axis = .horizontal
+        openCardPartSV.distribution = .equalSpacing
+        openCardPartSV.addArrangedSubview(openLabelPart)
+        openCardPartSV.addArrangedSubview(openDataPart)
+        
+        highLabelPart.title = "H"
+        highLabelPart.titleColor = .systemGray
+        highLabelPart.titleFont = .preferredFont(forTextStyle: .subheadline)
+        highLabelPart.label.adjustsFontForContentSizeCategory = true
+        
+        highDataPart.title = "100,000,000"
+        highDataPart.titleColor = .label
+        highDataPart.titleFont = .preferredFont(forTextStyle: .subheadline)
+        highDataPart.label.adjustsFontForContentSizeCategory = true
+        
+        highCardPartSV.axis = .horizontal
+        highCardPartSV.distribution = .equalSpacing
+        highCardPartSV.addArrangedSubview(highLabelPart)
+        highCardPartSV.addArrangedSubview(highDataPart)
+        
+        lowLabelPart.title = "L"
+        lowLabelPart.titleColor = .systemGray
+        lowLabelPart.titleFont = .preferredFont(forTextStyle: .subheadline)
+        lowLabelPart.label.adjustsFontForContentSizeCategory = true
+        
+        lowDataPart.title = "100,000,000"
+        lowDataPart.titleColor = .label
+        lowDataPart.titleFont = .preferredFont(forTextStyle: .subheadline)
+        lowDataPart.label.adjustsFontForContentSizeCategory = true
+        
+        lowCardPartSV.axis = .horizontal
+        lowCardPartSV.distribution = .equalSpacing
+        lowCardPartSV.addArrangedSubview(lowLabelPart)
+        lowCardPartSV.addArrangedSubview(lowDataPart)
+        
+        closeLabelPart.title = "C"
+        closeLabelPart.titleColor = .systemGray
+        closeLabelPart.titleFont = .preferredFont(forTextStyle: .subheadline)
+        closeLabelPart.label.adjustsFontForContentSizeCategory = true
+        
+        closeDataPart.title = "100,000,000"
+        closeDataPart.titleColor = .label
+        closeDataPart.titleFont = .preferredFont(forTextStyle: .subheadline)
+        closeDataPart.label.adjustsFontForContentSizeCategory = true
+        
+        closeCardPartSV.axis = .horizontal
+        closeCardPartSV.distribution = .equalSpacing
+        closeCardPartSV.addArrangedSubview(closeLabelPart)
+        closeCardPartSV.addArrangedSubview(closeDataPart)
+    }
+    
+    private func configCardPartSV() {
+        cardPartSV.axis = .vertical
+        cardPartSV.spacing = 5
+        cardPartSV.distribution = .equalSpacing
+        cardPartSV.margins = UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15)
+        cardPartSV.addArrangedSubview(titlePart)
+        cardPartSV.addArrangedSubview(openCardPartSV)
+        cardPartSV.addArrangedSubview(highCardPartSV)
+        cardPartSV.addArrangedSubview(lowCardPartSV)
+        cardPartSV.addArrangedSubview(closeCardPartSV)
+    }
+}
