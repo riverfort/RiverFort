61a776cee8c505d4b1ed9578d79ab41f82903b7e
Qiuyang Nie <qyang.nie@gmail.com>
2021-09-02 10:57:33 +0100

    Add fetchHistoricalPriceFromRiverFort

--- RiverFort/Application/DetailView-v2/Model/DetailViewAPIFunction.swift
+++ RiverFort/Application/DetailView-v2/Model/DetailViewAPIFunction.swift
@@ -7,3 +7,9 @@
 
 import Foundation
 import Alamofire
+
+class DetailViewAPIFunction {
+    static func fetchHistoricalPriceFromRiverFort(symbol: String) {
+        AF.request("https://data.riverfort.com/api/v1/companies/\(symbol)/trading?ordering=market_date").validate()
+    }
+}
