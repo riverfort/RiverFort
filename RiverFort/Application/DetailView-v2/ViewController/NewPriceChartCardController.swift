b5fde5aa9ef1ab31daa3b49aa1b2c68cf886d64f
Qiuyang Nie <qyang.nie@gmail.com>
2021-09-03 13:36:30 +0100

    Add createObservesr

--- RiverFort/Application/DetailView-v2/ViewController/NewPriceChartCardController.swift
+++ RiverFort/Application/DetailView-v2/ViewController/NewPriceChartCardController.swift
@@ -8,8 +8,35 @@
 import UIKit
 
 class NewPriceChartCardController: TemplateCardController {
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        createObservesr()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupCardParts([NewPriceChartCardPartView()])
+    }
+}
+
+extension NewPriceChartCardController {
+    private func createObservesr() {
+        let aName = Notification.Name(NewSearchConstant.SELECT_SEARCH_COMPANY)
+        NotificationCenter.default.addObserver(self, selector: #selector(prepareView), name: aName, object: nil)
+    }
+    
+    @objc private func prepareView(notification: Notification) {
+        guard let company = notification.object as? YahooFinanceSearchedCompany else {
+            return
+        }
+        print(company)
     }
 }
