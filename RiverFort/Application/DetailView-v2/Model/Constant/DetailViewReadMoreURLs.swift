45dd2a713ab95c3a7ef3dd93e2c1a800881153ae
Qiuyang Nie <qyang.nie@gmail.com>
2021-09-05 12:54:15 +0100

    Add DetailViewReadMoreURLs

---
+++ RiverFort/Application/DetailView-v2/Model/Constant/DetailViewReadMoreURLs.swift
@@ -0,0 +1,12 @@
+//
+//  DetailViewReadMoreURLs.swift
+//  RiverFort
+//
+//  Created by Qiuyang Nie on 05/09/2021.
+//
+
+import Foundation
+
+struct DetailViewReadMoreURLs {
+    static var AQUIS_EXCHANGE = "https://www.aquis.eu/aquis-stock-exchange/member?securityidaqse="
+}
