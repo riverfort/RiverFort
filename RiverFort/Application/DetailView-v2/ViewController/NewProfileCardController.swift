71b1172e1f1128ba2fad6740f6d2b1fe46988836
Qiuyang Nie <qyang.nie@gmail.com>
2021-09-05 10:08:37 +0100

    Add industry and sector

--- RiverFort/Application/DetailView-v2/ViewController/NewProfileCardController.swift
+++ RiverFort/Application/DetailView-v2/ViewController/NewProfileCardController.swift
@@ -10,6 +10,13 @@ import CardParts
 
 class NewProfileCardController: TemplateCardController {
     private let titlePart = CardPartTitleView(type: .titleOnly)
+    
+    private let industryLabelPart = CardPartTitleView(type: .titleOnly)
+    private let industryDataPart = CardPartTitleView(type: .titleOnly)
+
+    private let sectorLabelPart = CardPartTitleView(type: .titleOnly)
+    private let sectorDataPart = CardPartTitleView(type: .titleOnly)
+    
     private let cardPartSV = CardPartStackView()
     
     override func viewDidLoad() {
@@ -25,6 +32,30 @@ extension NewProfileCardController {
         titlePart.titleColor = .label
         titlePart.titleFont = .preferredFont(forTextStyle: .headline)
         titlePart.label.adjustsFontForContentSizeCategory = true
+        
+        industryLabelPart.title = "Industry"
+        industryLabelPart.label.numberOfLines = 0
+        industryLabelPart.titleColor = .systemGray
+        industryLabelPart.titleFont = .preferredFont(forTextStyle: .subheadline)
+        industryLabelPart.label.adjustsFontForContentSizeCategory = true
+        
+        industryDataPart.title = "-"
+        industryDataPart.label.numberOfLines = 0
+        industryDataPart.titleColor = .label
+        industryDataPart.titleFont = .preferredFont(forTextStyle: .subheadline)
+        industryDataPart.label.adjustsFontForContentSizeCategory = true
+        
+        sectorLabelPart.title = "Sector"
+        sectorLabelPart.label.numberOfLines = 0
+        sectorLabelPart.titleColor = .systemGray
+        sectorLabelPart.titleFont = .preferredFont(forTextStyle: .subheadline)
+        sectorLabelPart.label.adjustsFontForContentSizeCategory = true
+        
+        sectorDataPart.title = "-"
+        sectorDataPart.label.numberOfLines = 0
+        sectorDataPart.titleColor = .label
+        sectorDataPart.titleFont = .preferredFont(forTextStyle: .subheadline)
+        sectorDataPart.label.adjustsFontForContentSizeCategory = true
     }
     
     private func configStackView() {
@@ -33,6 +64,10 @@ extension NewProfileCardController {
         cardPartSV.distribution = .equalSpacing
         cardPartSV.margins = UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15)
         cardPartSV.addArrangedSubview(titlePart)
+        cardPartSV.addArrangedSubview(industryLabelPart)
+        cardPartSV.addArrangedSubview(industryDataPart)
+        cardPartSV.addArrangedSubview(sectorLabelPart)
+        cardPartSV.addArrangedSubview(sectorDataPart)
     }
     
     private func configCardParts() {
