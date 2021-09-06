00cecc4f53ff61b6803c19e3b263f4954f4c2676
Qiuyang Nie <qyang.nie@gmail.com>
2021-09-06 10:24:16 +0100

    Scaffold NewsViewModel

--- RiverFort/Application/DetailView-v2/Model/NewsViewModel.swift
+++ RiverFort/Application/DetailView-v2/Model/NewsViewModel.swift
@@ -5,8 +5,10 @@
 //  Created by Qiuyang Nie on 06/09/2021.
 //
 
-import Foundation
+import RxSwift
+import RxCocoa
 
 class NewsViewModel {
-    
+    private let rssFeedParser = RSSFeedParser()
+    private let rssItems: BehaviorRelay<[RSSItem]> = BehaviorRelay(value: [])
 }
