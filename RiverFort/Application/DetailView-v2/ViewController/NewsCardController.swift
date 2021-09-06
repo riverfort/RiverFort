0d826413b745edcabe9c701ba2ec5b11606cc3df
Qiuyang Nie <qyang.nie@gmail.com>
2021-09-06 10:09:44 +0100

    Scaffold structure NewsCardController

--- RiverFort/Application/DetailView-v2/ViewController/NewsCardController.swift
+++ RiverFort/Application/DetailView-v2/ViewController/NewsCardController.swift
@@ -6,9 +6,40 @@
 //
 
 import UIKit
+import CardParts
 
 class NewsCardController: TemplateCardController {
+    private let titlePart = CardPartTitleView(type: .titleOnly)
+    private let newsTableView = CardPartTableView()
+    private let cardPartSV = CardPartStackView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configCardParts()
+    }
+}
+
+extension NewsCardController {
+    private func configTitleView() {
+        titlePart.title = "News"
+        titlePart.label.numberOfLines = 0
+        titlePart.titleColor = .label
+        titlePart.titleFont = .preferredFont(forTextStyle: .headline)
+        titlePart.label.adjustsFontForContentSizeCategory = true
+    }
+    
+    private func configStackView() {
+        cardPartSV.axis = .vertical
+        cardPartSV.spacing = 5
+        cardPartSV.distribution = .equalSpacing
+        cardPartSV.margins = UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15)
+        cardPartSV.addArrangedSubview(titlePart)
+        cardPartSV.addArrangedSubview(newsTableView)
+    }
+    
+    private func configCardParts() {
+        configTitleView()
+        configStackView()
+        setupCardParts([cardPartSV])
     }
 }
