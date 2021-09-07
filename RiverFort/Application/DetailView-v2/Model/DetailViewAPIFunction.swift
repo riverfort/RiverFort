e992109ad0d3bfd2630ca59c22ef7b41c3652755
Qiuyang Nie <qyang.nie@gmail.com>
2021-09-07 08:22:54 +0100

    Remove fetchUKNewsfeed

--- RiverFort/Application/DetailView-v2/Model/DetailViewAPIFunction.swift
+++ RiverFort/Application/DetailView-v2/Model/DetailViewAPIFunction.swift
@@ -13,10 +13,6 @@ class DetailViewAPIFunction {
         AF.request("https://data.riverfort.com/api/v1/companies/\(symbol)/trading?ordering=market_date").validate()
     }
     
-    static func fetchUKNewsfeed(symbol: String) -> DataRequest {
-        AF.request("https://www.investegate.co.uk/Rss.aspx?company=\(symbol)").validate()
-    }
-    
     static func fetchQuoteFromYahooFinance(symbol: String) -> DataRequest {
         AF.request("https://query2.finance.yahoo.com/v7/finance/options/\(symbol)").validate()
     }
