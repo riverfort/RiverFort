bcf7b5aa217c65cab91a0a6919a0acd0ef329fd4
Qiuyang Nie <qyang.nie@gmail.com>
2021-09-07 08:41:36 +0100

    Add fetchHistPriceFromFMP

--- RiverFort/Application/DetailView-v2/Model/DetailViewAPIFunction.swift
+++ RiverFort/Application/DetailView-v2/Model/DetailViewAPIFunction.swift
@@ -9,13 +9,35 @@ import Foundation
 import Alamofire
 
 class DetailViewAPIFunction {
-    static func fetchHistoricalPriceFromRiverFort(symbol: String) -> DataRequest {
-        AF.request("https://data.riverfort.com/api/v1/companies/\(symbol)/trading?ordering=market_date").validate()
-    }
-    
     static func fetchQuoteFromYahooFinance(symbol: String) -> DataRequest {
         AF.request("https://query2.finance.yahoo.com/v7/finance/options/\(symbol)").validate()
     }
+}
+
+extension DetailViewAPIFunction {
+    static func fetchHistPriceFromRiverFort(symbol: String) -> DataRequest {
+        AF.request("https://data.riverfort.com/api/v1/companies/\(symbol)/trading?ordering=market_date").validate()
+    }
+    
+    static func fetchHistPriceFromFMP(symbol: String, timeseries: Int) -> DataRequest {
+        AF.request("https://financialmodelingprep.com/api/v3/historical-price-full/\(symbol)?timeseries=\(timeseries)&apikey=2797db3c7193bf4ec7231be3cba5f27c").validate()
+    }
+}
+
+struct FMPHistPriceResult: Decodable {
+    struct FMPHistPrice: Decodable {
+        let date: String
+        let open: Double
+        let high: Double
+        let low: Double
+        let close: Double
+        let volume: Double
+        let change: Double
+        let changePercent: Double
+        let vwap: Double
+    }
+    let symbol: String
+    let historical: [FMPHistPrice]
 }
 
 struct YahooFinanceQuoteResult: Decodable {
