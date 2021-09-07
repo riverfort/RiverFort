69e6c0582dc1ea27dcee9c7d1c12a64a484d5f76
Qiuyang Nie <qyang.nie@gmail.com>
2021-09-07 09:51:22 +0100

    Scaffold TimeseriesCardControllerViewController

---
+++ RiverFort/Application/DetailView-v2/ViewController/TimeseriesCardControllerViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TimeseriesCardControllerViewController.swift
+//  RiverFort
+//
+//  Created by Qiuyang Nie on 07/09/2021.
+//
+
+import UIKit
+import CardParts
+
+class TimeseriesCardControllerViewController: TemplateCardController {    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension TimeseriesCardControllerViewController {
+    private func configCardParts() {}
+}
