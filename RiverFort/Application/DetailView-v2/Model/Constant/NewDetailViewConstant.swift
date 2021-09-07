08223aa55d8f87c2f91f52def4d60c32fef88ced
Qiuyang Nie <qyang.nie@gmail.com>
2021-09-07 16:28:18 +0100

    Add constants

--- RiverFort/Application/DetailView-v2/Model/Constant/NewDetailViewConstant.swift
+++ RiverFort/Application/DetailView-v2/Model/Constant/NewDetailViewConstant.swift
@@ -9,6 +9,8 @@ import Foundation
 
 struct NewDetailViewConstant {
     static var TIMESERIES_CHANGED = "com.riverfort.timeseriesChanged"
+    static var CHART_VALUE_SELECTED = "com.riverfort.chartValueSelected"
+    static var CHART_VALUE_NO_LONGER_SELECTED = "com.riverfort.chartValueNoLongerSelected"
     static var YAHOO_FINANCE_QUOTE_RESULT = "com.riverfort.yahooFinanceQuoteResult"
     static var FMP_HIST_PRICE_RESULT = "com.riverfort.fmpHistPriceResult"
 }
