15e67db68fdcaae85f3ff5d880d9f7d668c2eb8b
Qiuyang Nie <qyang.nie@gmail.com>
2021-09-08 10:11:38 +0100

    Add menu

--- RiverFort/Application/DetailView-v2/ViewController/NewCompanyDetailViewController.swift
+++ RiverFort/Application/DetailView-v2/ViewController/NewCompanyDetailViewController.swift
@@ -50,9 +50,22 @@ extension NewCompanyDetailViewController {
         let configuration = UIImage.SymbolConfiguration(weight: .semibold)
         let add = UIButton(type: .system)
         add.setImage(UIImage(systemName: "plus.circle", withConfiguration: configuration), for: .normal)
+        
+        var menu: UIMenu {
+            return UIMenu(title: "Share Price Chart", image: nil, identifier: nil, options: [], children: menuItems)
+        }
+        var menuItems: [UIAction] {
+            return [
+                UIAction(title: "Price & Vol", image: UIImage(systemName: "chart.bar"), handler: { (_) in
+                }),
+                UIAction(title: "News", image: UIImage(systemName: "newspaper"), handler: { (_) in
+                }),
+            ]
+        }
         let more = UIButton(type: .system)
         more.setImage(UIImage(systemName: "ellipsis.circle", withConfiguration: configuration), for: .normal)
-        more.addTarget(self, action: #selector(onDidTapMoreButton), for: .touchUpInside)
+        more.menu = menu
+        more.showsMenuAsPrimaryAction = true
         
         let stackview = UIStackView.init(arrangedSubviews: [add, more])
         stackview.distribution = .equalSpacing
@@ -60,12 +73,6 @@ extension NewCompanyDetailViewController {
         stackview.alignment = .center
         stackview.spacing = 8
         navigationItem.rightBarButtonItem = UIBarButtonItem(customView: stackview)
-    }
-}
-
-extension NewCompanyDetailViewController {
-    @objc private func onDidTapMoreButton() {
-        print("hello")
     }
 }
 
