dd1fc33baf90669045b28771a2fb4789fa937eca
Qiuyang Nie <qyang.nie@gmail.com>
2021-09-10 13:57:22 +0100

    Scaffold NewADTVMarker

---
+++ RiverFort/Application/DetailView-v2/View/NewADTVMarker.swift
@@ -0,0 +1,67 @@
+//
+//  NewADTVMarker.swift
+//  RiverFort
+//
+//  Created by Qiuyang Nie on 10/09/2021.
+//
+
+import Charts
+
+class NewADTVMarker: MarkerImage {
+    @objc var color: UIColor = .systemRed
+    @objc var radius: CGFloat = 5
+    private var price = "-"
+    private var date = "-"
+    private var priceAttributes = [NSAttributedString.Key : Any]()
+    private var dateAttributes = [NSAttributedString.Key : Any]()
+
+    override func draw(context: CGContext, point: CGPoint) {
+        configView()
+        drawPrice(text: "\(price)" as NSString,
+                  rect: CGRect(origin: CGPoint(x: point.x, y: point.y), size: size),
+                  withAttributes: priceAttributes)
+        drawDate(text: "\(date)" as NSString,
+                 rect: CGRect(origin: CGPoint(x: point.x, y: point.y), size: size),
+                 withAttributes: dateAttributes)
+        drawHighlightPoint(context: context, point: point)
+    }
+    
+    override func refreshContent(entry: ChartDataEntry, highlight: Highlight) {
+        super.refreshContent(entry: entry, highlight: highlight)
+        guard let histPriceChartDataEntryData = entry.data as? HistPriceChartDataEntryData else {
+            return
+        }
+        price = "\(entry.y) (\(histPriceChartDataEntryData.volume.withCommas()))"
+        date = DateFormatterUtils.convertDateFormater(histPriceChartDataEntryData.date)
+    }
+}
+
+extension NewADTVMarker {
+    private func configView() {
+        priceAttributes[.font] = UIFont.preferredFont(forTextStyle: .headline)
+        priceAttributes[.foregroundColor] = UIColor.label
+        dateAttributes[.font] = UIFont.preferredFont(forTextStyle: .subheadline)
+        dateAttributes[.foregroundColor] = UIColor.secondaryLabel
+    }
+}
+
+extension NewADTVMarker {
+    private func drawPrice(text: NSString, rect: CGRect, withAttributes attributes: [NSAttributedString.Key : Any]? = nil) {
+        let size = text.size(withAttributes: attributes)
+        let centeredRect = CGRect(x: 15, y: 0, width: size.width, height: size.height)
+        text.draw(in: centeredRect, withAttributes: attributes)
+    }
+    
+    private func drawDate(text: NSString, rect: CGRect, withAttributes attributes: [NSAttributedString.Key : Any]? = nil) {
+        let size = text.size(withAttributes: attributes)
+        let centeredRect = CGRect(x: 15, y: 25, width: size.width, height: size.height)
+        text.draw(in: centeredRect, withAttributes: attributes)
+    }
+    
+    private func drawHighlightPoint(context: CGContext, point: CGPoint) {
+        let circleRect = CGRect(x: point.x - radius, y: point.y - radius, width: radius * 2, height: radius * 2)
+        context.setFillColor(color.cgColor)
+        context.fillEllipse(in: circleRect)
+        context.restoreGState()
+    }
+}
