d2423813c455cd82ce4d3b2da142004714e74033
Qiuyang Nie <qyang.nie@gmail.com>
2021-09-13 12:08:55 +0100

    Update properties with optional

--- RiverFort/Application/DetailView-v2/Model/ChartModel/ChartDataEntryData.swift
+++ RiverFort/Application/DetailView-v2/Model/ChartModel/ChartDataEntryData.swift
@@ -10,8 +10,20 @@ import Foundation
 struct HistPriceChartDataEntryData {
     let date: String
     let volume: Double
-    let change: Double?
-    let changePercent: Double?
+    var change: Double? = nil
+    var changePercent: Double? = nil
+    
+    init(date: String, volume: Double) {
+        self.date = date
+        self.volume = volume
+    }
+    
+    init(date: String, volume: Double, change: Double, changePercent: Double) {
+        self.date = date
+        self.volume = volume
+        self.change = change
+        self.changePercent = changePercent
+    }
 }
 
 struct NewsChartDataEntryData {
