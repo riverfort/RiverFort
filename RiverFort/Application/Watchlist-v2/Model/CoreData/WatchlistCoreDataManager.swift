c3b0a5b6a46d713842294af61df428fc5cfff26c
Qiuyang Nie <qyang.nie@gmail.com>
2021-09-14 15:20:42 +0100

    Add WatchlistCoreDataManager

---
+++ RiverFort/Application/Watchlist-v2/Model/CoreData/WatchlistCoreDataManager.swift
@@ -0,0 +1,24 @@
+//
+//  WatchlistCoreDataManager.swift
+//  RiverFort
+//
+//  Created by Qiuyang Nie on 14/09/2021.
+//
+
+import CoreData
+
+class WatchlistCoreDataManager {
+    private static let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    static func getWatchedCompanies() -> [WatchedCompany]? {
+        do {
+            let request = WatchedCompany.fetchRequest() as NSFetchRequest<WatchedCompany>
+            let sort = NSSortDescriptor(key: "rowOrder", ascending: true)
+            request.sortDescriptors = [sort]
+            let watchedCompanies = try context.fetch(request)
+            return watchedCompanies
+        } catch {
+            return nil
+        }
+    }
+}
