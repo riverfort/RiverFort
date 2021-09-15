93bd5cf766a241c15f589ed7bd7c9f3e6126dfcc
Qiuyang Nie <qyang.nie@gmail.com>
2021-09-15 12:41:44 +0100

    Refactor

--- RiverFort/Application/Main/HomeViewController.swift
+++ RiverFort/Application/Main/HomeViewController.swift
@@ -10,24 +10,20 @@ import UIKit
 class HomeViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        let searchV2VC = UINavigationController(rootViewController: SearchV2ViewController())
-        let newWatchlistVC = UINavigationController(rootViewController: NewWatchlistViewController())
-        let searchVC = UINavigationController(rootViewController: SearchViewController())
-        let watchlistVC = UINavigationController(rootViewController: WatchlistViewController())
+        let searchVC = UINavigationController(rootViewController: SearchV2ViewController())
+        let watchlistVC = UINavigationController(rootViewController: NewWatchlistViewController())
         let settingsVC = UINavigationController(rootViewController: SettingsViewController())
         
-        searchV2VC.title = "Search"
-        newWatchlistVC.title = "Watchlist"
         searchVC.title = "Search"
         watchlistVC.title = "Watchlist"
         settingsVC.title = "Settings"
 
-        self.setViewControllers([searchV2VC, newWatchlistVC, searchVC, watchlistVC, settingsVC], animated: true)
+        self.setViewControllers([searchVC, watchlistVC, settingsVC], animated: true)
         guard let items = self.tabBar.items else {
             return
         }
         
-        let images = ["magnifyingglass.circle.fill", "heart.text.square.fill", "magnifyingglass.circle.fill", "heart.text.square.fill", "gear"]
+        let images = ["magnifyingglass.circle.fill", "heart.text.square.fill", "gear"]
         
         let configuration = UIImage.SymbolConfiguration(weight: .heavy)
         for x in 0..<items.count {
