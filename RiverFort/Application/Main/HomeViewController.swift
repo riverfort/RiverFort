2f2b1612858c814c63b0979dcf3783b981972622
Qiuyang Nie <qyang.nie@gmail.com>
2021-09-15 12:55:29 +0100

    Refactor

--- RiverFort/Application/Main/HomeViewController.swift
+++ RiverFort/Application/Main/HomeViewController.swift
@@ -11,19 +11,17 @@ class HomeViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let searchVC = UINavigationController(rootViewController: SearchV2ViewController())
-        let watchlistVC = UINavigationController(rootViewController: NewWatchlistViewController())
         let settingsVC = UINavigationController(rootViewController: SettingsViewController())
         
         searchVC.title = "Search"
-        watchlistVC.title = "Watchlist"
         settingsVC.title = "Settings"
 
-        self.setViewControllers([searchVC, watchlistVC, settingsVC], animated: true)
+        self.setViewControllers([searchVC, settingsVC], animated: true)
         guard let items = self.tabBar.items else {
             return
         }
         
-        let images = ["magnifyingglass.circle.fill", "heart.text.square.fill", "gear"]
+        let images = ["magnifyingglass.circle.fill", "gear"]
         
         let configuration = UIImage.SymbolConfiguration(weight: .heavy)
         for x in 0..<items.count {
