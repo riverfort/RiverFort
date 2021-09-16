68af570cc33d626910a6f323751e0ad747a3bef8
Qiuyang Nie <qyang.nie@gmail.com>
2021-09-16 09:17:40 +0100

    Refactor HomeTabBarController

--- RiverFort/Features/Home/HomeTabBarController.swift
+++ RiverFort/Features/Home/HomeTabBarController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class HomeTabBarController: UITabBarController {
+    // MARK: - Add new tabs here:
+    
     private lazy var tabBarImages = ["magnifyingglass.circle.fill", "gear"]
     private lazy var searchVC   = UINavigationController(rootViewController: SearchV2ViewController())
     private lazy var settingsVC = UINavigationController(rootViewController: SettingsViewController())
@@ -17,14 +19,20 @@ extension HomeTabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configView()
+        configTabBar()
     }
 }
 
 extension HomeTabBarController {
+    // MARK: - Config new tabs here:
+
     private func configView() {
         searchVC.title   = "Search"
         settingsVC.title = "Settings"
         setViewControllers([searchVC, settingsVC], animated: true)
+    }
+    
+    private func configTabBar() {
         tabBar.tintColor = .systemIndigo
         guard let items = tabBar.items else { return }
         for i in 0..<items.count { items[i].image = UIImage(systemName: tabBarImages[i], withConfiguration: .heavy) }
