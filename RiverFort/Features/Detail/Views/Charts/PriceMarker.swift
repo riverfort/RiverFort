9f55593dc3cee171d947a3efeb7d5537e3b2aea0
Qiuyang Nie <qyang.nie@gmail.com>
2021-09-21 13:16:07 +0100

    Refactor PriceMarker

--- RiverFort/Features/Detail/Views/Charts/PriceMarker.swift
+++ RiverFort/Features/Detail/Views/Charts/PriceMarker.swift
@@ -29,10 +29,16 @@ class PriceMarker: MarkerImage {
     override func refreshContent(entry: ChartDataEntry, highlight: Highlight) {
         super.refreshContent(entry: entry, highlight: highlight)
         guard let histPriceChartDataEntryData = entry.data as? HistoricalPriceChartDataEntryData else { return }
-        price = String(format: "%.2f", entry.y) + " " + "(\(histPriceChartDataEntryData.volume.withCommas()))"
         let dateFormatter = DateFormatter()
         dateFormatter.dateStyle = .medium
         dateFormatter.timeStyle = .none
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.maximumFractionDigits = 2
+        if let price = numberFormatter.string(from: NSNumber(value: entry.y)),
+           let vol = numberFormatter.string(from: NSNumber(value: histPriceChartDataEntryData.volume)) {
+            self.price = price + " (" + vol + ")"
+        }
         let datetime = dateFormatter.string(from: histPriceChartDataEntryData.date)
         date = datetime
     }
