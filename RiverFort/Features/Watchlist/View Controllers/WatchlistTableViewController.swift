ba8a3c82086e33b09377102bedf4784970104466
Qiuyang Nie <qyang.nie@gmail.com>
2021-09-25 13:07:04 +0100

    Add RealmSwift to scaffold WatchlistTableViewController

--- RiverFort/Features/Watchlist/View Controllers/WatchlistTableViewController.swift
+++ RiverFort/Features/Watchlist/View Controllers/WatchlistTableViewController.swift
@@ -6,11 +6,16 @@
 //
 
 import UIKit
+import RealmSwift
 
 class WatchlistTableViewController: UITableViewController {
+    private let realm = try! Realm()
+    private lazy var watchlistCompanies = realm.objects(WatchlistCompany.self)
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configNavigationController()
+        configTableView()
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -22,24 +27,19 @@ class WatchlistTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return watchlistCompanies.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = watchlistCompanies[indexPath.row].symbol
         return cell
     }
-    */
+
 
     /*
     // Override to support conditional editing of the table view.
@@ -86,4 +86,19 @@ class WatchlistTableViewController: UITableViewController {
     }
     */
 
+}
+
+extension WatchlistTableViewController {
+    private func configNavigationController() {
+        navigationItem.title = "Watchlist"
+        navigationItem.largeTitleDisplayMode = .always
+        navigationItem.hidesSearchBarWhenScrolling = false
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.tintColor = .systemIndigo
+    }
+    
+    private func configTableView() {
+        tableView = UITableView(frame: tableView.frame, style: .insetGrouped)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
 }
