b970e42b149feddfb69420e73d07178184bef98c
Qiuyang Nie <qyang.nie@gmail.com>
2021-09-27 13:34:10 +0100

    Add AppDelegate+PushNotification

--- RiverFort/Features/Main/AppDelegate.swift
+++ RiverFort/Features/Main/AppDelegate.swift
@@ -11,10 +11,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        UNUserNotificationCenter.current().delegate = self
-        
-        PushNotifications.registerForPushNotifications()
-        PushNotifications.handleLaunchFromNotification(didFinishLaunchingWithOptions: launchOptions)
+        configPushNotification(didFinishLaunchingWithOptions: launchOptions)
         return true
     }
     
@@ -26,37 +23,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) { }
 
-}
-
-extension AppDelegate {
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
-        let token = tokenParts.joined()
-        print("Device Token: \(token)")
-    }
-    
-    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-        print("Failed to register: \(error)")
-    }
-}
-
-extension AppDelegate {
-    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
-                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        guard let aps = userInfo["aps"] as? [String: AnyObject] else { completionHandler(.failed); return }
-        print("didReceiveRemoteNotification: \(aps)")
-    }
-}
-
-// MARK: - UNUserNotificationCenterDelegate
-
-extension AppDelegate: UNUserNotificationCenterDelegate {
-    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse,
-                                withCompletionHandler completionHandler: @escaping () -> Void) {
-        let userInfo = response.notification.request.content.userInfo
-        if let aps = userInfo["aps"] as? [String: AnyObject] {
-            if response.actionIdentifier == "view_notification" { print("view_notification: \(aps)") }
-        }
-        completionHandler()
-    }
 }
