617d0b7ec8624208233f91c1fffcaa8d4082dc81
Qiuyang Nie <qyang.nie@gmail.com>
2021-09-28 12:56:24 +0100

    Update url of syncWatchlist

--- RiverFort/Features/Watchlist/Models/Networking/WatchlistAPIFunction.swift
+++ RiverFort/Features/Watchlist/Models/Networking/WatchlistAPIFunction.swift
@@ -29,7 +29,7 @@ class WatchlistAPIFunction {
             .filter{ $0.exchange == "London" || $0.exchange == "AQS" }
             .map { ["device_token": deviceToken, "company_ticker": $0.symbol] }
         watchlist.forEach { watchlistItem in
-            AF.request("http://127.0.0.1:8000/watchlist/v1", method: .post, parameters: watchlistItem).validate().response { response in
+            AF.request("https://data.riverfort.com/watchlist/v1", method: .post, parameters: watchlistItem).validate().response { response in
                 switch response.result {
                 case .success: print("watchlist item synced: \(watchlistItem)")
                 case .failure(let error):
