a817f45f7b21e68a3c5c8beda24b0c896630bcbf
Qiuyang Nie <qyang.nie@gmail.com>
2021-10-02 12:05:25 +0100

    Add deviceToken user defaults key

--- RiverFort/Extensions/UserDefaultsExtensions.swift
+++ RiverFort/Extensions/UserDefaultsExtensions.swift
@@ -10,5 +10,6 @@ import Foundation
 extension UserDefaults {
     struct Keys {
         static let timeseriesSelectedSegmentIndex = "timeseries_selected_segment_index"
+        static let deviceToken = "device_token"
     }
 }
