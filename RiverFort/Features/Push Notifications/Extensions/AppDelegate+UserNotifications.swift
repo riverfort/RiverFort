49b6c6595bed51bd74ca3a9699553bb2a15f4571
Qiuyang Nie <qyang.nie@gmail.com>
2021-10-04 09:12:34 +0100

    Scaffold AppDelegate+UserNotifications

---
+++ RiverFort/Features/Push Notifications/Extensions/AppDelegate+UserNotifications.swift
@@ -0,0 +1,49 @@
+//
+//  AppDelegate+UserNotifications.swift
+//  RiverFort
+//
+//  Created by Qiuyang Nie on 04/10/2021.
+//
+
+import UIKit
+import UserNotifications
+
+
+// MARK: - Asking for User Notifications Permission
+
+extension AppDelegate {
+    func registerForPushNotifications() {
+        UNUserNotificationCenter.current()
+            .requestAuthorization(options: [.alert, .sound, .badge]) { [weak self] granted, _ in
+                print("Permission granted: \(granted)")
+                guard granted else { return }
+                self?.getNotificationSettings()
+            }
+    }
+    
+    private func getNotificationSettings() {
+        UNUserNotificationCenter.current().getNotificationSettings { settings in
+            print("Notification settings: \(settings)")
+            guard settings.authorizationStatus == .authorized else { return }
+            DispatchQueue.main.async {
+                UIApplication.shared.registerForRemoteNotifications()
+            }
+        }
+    }
+    
+
+}
+
+// MARK: - Registering With APNs
+
+extension AppDelegate {
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
+        let token = tokenParts.joined()
+        print("Device Token: \(token)")
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Failed to register: \(error)")
+    }
+}
