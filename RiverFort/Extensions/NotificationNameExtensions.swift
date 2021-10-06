b4336686c9d2adfb3fbe2fa4626f3292446afb02
Qiuyang Nie <qyang.nie@gmail.com>
2021-10-06 13:07:37 +0100

    Add notification name hasUpdatedChartMode

--- RiverFort/Extensions/NotificationNameExtensions.swift
+++ RiverFort/Extensions/NotificationNameExtensions.swift
@@ -19,6 +19,7 @@ extension Notification.Name {
     static let didReceiveHistoricalADTV60 = Notification.Name("did_receive_historical_adtv60")
     static let didReceiveHistoricalADTV120 = Notification.Name("did_receive_historical_adtv120")
     
+    static let hasUpdatedChartMode = Notification.Name("has_updated_chart_mode")
     static let hasUpdatedTimeSeries = Notification.Name("has_updated_timeseries")
     
     static let didSelectChartValue = Notification.Name("did_select_chart_value")
