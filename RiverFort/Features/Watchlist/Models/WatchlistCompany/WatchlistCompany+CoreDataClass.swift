78992df8b7b0177172bc441561b5887cff2e8c4b
Qiuyang Nie <qyang.nie@gmail.com>
2021-10-11 12:58:18 +0100

    Set properties non-optional

---
+++ RiverFort/Features/Watchlist/Models/WatchlistCompany/WatchlistCompany+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  WatchlistCompany+CoreDataClass.swift
+//  
+//
+//  Created by Qiuyang Nie on 11/10/2021.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(WatchlistCompany)
+public class WatchlistCompany: NSManagedObject {
+
+}
---
+++ RiverFort/Features/Watchlist/Models/WatchlistCompany/WatchlistCompany+CoreDataProperties.swift
@@ -0,0 +1,24 @@
+//
+//  WatchlistCompany+CoreDataProperties.swift
+//  
+//
+//  Created by Qiuyang Nie on 11/10/2021.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension WatchlistCompany {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<WatchlistCompany> {
+        return NSFetchRequest<WatchlistCompany>(entityName: "WatchlistCompany")
+    }
+
+    @NSManaged public var displayOrder: Int16
+    @NSManaged public var exchange: String?
+    @NSManaged public var name: String?
+    @NSManaged public var symbol: String?
+
+}
