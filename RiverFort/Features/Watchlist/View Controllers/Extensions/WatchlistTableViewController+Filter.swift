5c7e4080f7012df12febcb908e170a59607ba731
Qiuyang Nie <qyang.nie@gmail.com>
2021-10-13 09:05:23 +0100

    Add WatchlistTableViewController+Filter

---
+++ RiverFort/Features/Watchlist/View Controllers/Extensions/WatchlistTableViewController+Filter.swift
@@ -0,0 +1,12 @@
+//
+//  WatchlistTableViewController+Filter.swift
+//  RiverFort
+//
+//  Created by Qiuyang Nie on 13/10/2021.
+//
+
+import Foundation
+
+extension WatchlistTableViewController {
+    
+}
