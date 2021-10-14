d88828c94594162becf627ffb5847e4fdd681832
Qiuyang Nie <qyang.nie@gmail.com>
2021-10-14 09:15:52 +0100

    Implement table view data source

--- RiverFort/Features/Watchlist/View Controllers/WatchlistFiltersTableViewController.swift
+++ RiverFort/Features/Watchlist/View Controllers/WatchlistFiltersTableViewController.swift
@@ -27,24 +27,20 @@ class WatchlistFiltersTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        let exchanges = watchlistCompanyList!.watchlistCompanies.map { $0.exchange }
+        return exchanges.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        if cell == nil { cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell") }
+        cell!.textLabel?.text = "Hello"
+        return cell!
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
