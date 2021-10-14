bab1c1b2481a046fdbac2a3d8bd7409cea41fb04
Qiuyang Nie <qyang.nie@gmail.com>
2021-10-14 12:52:57 +0100

    Add viewWillDisappear

--- RiverFort/Features/Watchlist/View Controllers/WatchlistFiltersTableViewController.swift
+++ RiverFort/Features/Watchlist/View Controllers/WatchlistFiltersTableViewController.swift
@@ -30,6 +30,11 @@ class WatchlistFiltersTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("\(#function)")
+    }
 
     // MARK: - Table view data source
 
