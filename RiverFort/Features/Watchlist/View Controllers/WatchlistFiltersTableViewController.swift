7b21c601b9926a6c8b6387ff99fe56f209f11005
Qiuyang Nie <qyang.nie@gmail.com>
2021-10-20 09:30:02 +0100

    Modify access for exchanges

--- RiverFort/Features/Watchlist/View Controllers/WatchlistFiltersTableViewController.swift
+++ RiverFort/Features/Watchlist/View Controllers/WatchlistFiltersTableViewController.swift
@@ -19,7 +19,7 @@ protocol WatchlistFiltersTableViewControllerDelegate: AnyObject {
 
 class WatchlistFiltersTableViewController: UITableViewController {
     public weak var delegate: WatchlistFiltersTableViewControllerDelegate?
-    public lazy var exchanges: [FilteringExchange] = { () -> [FilteringExchange] in
+    private lazy var exchanges: [FilteringExchange] = { () -> [FilteringExchange] in
         let realm = try! Realm()
         let exchanges = Set(realm.objects(WatchlistCompanyList.self).first!.watchlistCompanies.map { $0.exchange })
             .sorted { $0 < $1 }
