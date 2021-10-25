9e2ff113cfc455602bbdddc865aef7ae91f26d41
Qiuyang Nie <qyang.nie@gmail.com>
2021-10-25 10:25:59 +0100

    Draft WatchlistTableViewController+Quote

---
+++ RiverFort/Features/Watchlist/View Controllers/Extensions/WatchlistTableViewController+/WatchlistTableViewController+Quote.swift
@@ -0,0 +1,20 @@
+//
+//  WatchlistTableViewController+Quote.swift
+//  RiverFort
+//
+//  Created by Qiuyang Nie on 25/10/2021.
+//
+
+import Foundation
+
+extension WatchlistTableViewController {
+    public func connectWatchlistCompaniesQuoteWebsocket() {
+        let watchlistCompanySymbols = Array(watchlistCompanies).map { $0.symbol }
+        let watchlistWebSocketController = WatchlistWebSocketController()
+        watchlistWebSocketController.subscribe(symbols: watchlistCompanySymbols)
+    }
+    
+    public func disconnectCompaniesQuoteWebsocket() {
+        
+    }
+}
