b7596f5277100458cd8e94e833c43f597808e423
Qiuyang Nie <qyang.nie@gmail.com>
2021-10-27 09:53:49 +0100

    Add implementation of WatchlistCell

--- RiverFort/Features/Watchlist/Views/WatchlistCell.swift
+++ RiverFort/Features/Watchlist/Views/WatchlistCell.swift
@@ -9,17 +9,101 @@ import UIKit
 
 class WatchlistCell: UITableViewCell {
     private let symbol = UILabel()
-    private let name = UILabel()
+    private let name   = UILabel()
+    private let price  = UILabel()
+    private let change = UILabel()
+    private let changePercent = UILabel()
+    private let stack = UIStackView()
+    private let profileStack = UIStackView()
+    private let statsStack = UIStackView()
+    private var regularConstraints: [NSLayoutConstraint] = []
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configStack()
+        configSymbolLabel()
+        configNameLabel()
+        configPriceLabel()
+        configChangeLabel()
+        setupLayoutConstraints()
+        updateLayoutConstraints()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
+}
 
+extension WatchlistCell {
+    private func configStack() {
+        addSubview(stack)
+        stack.addArrangedSubview(profileStack)
+        stack.addArrangedSubview(statsStack)
+        stack.axis = .horizontal
+        stack.distribution = .equalSpacing
+        configProfileStack()
+        configStatsStack()
+    }
+    
+    private func configProfileStack() {
+        profileStack.addArrangedSubview(symbol)
+        profileStack.addArrangedSubview(name)
+        profileStack.axis = .vertical
+        profileStack.distribution = .equalSpacing
+        profileStack.alignment = .leading
+        profileStack.spacing = 5
+    }
+    
+    private func configStatsStack() {
+        statsStack.addArrangedSubview(price)
+        statsStack.addArrangedSubview(change)
+        statsStack.axis = .vertical
+        statsStack.distribution = .equalSpacing
+        statsStack.alignment = .trailing
+        statsStack.spacing = 5
+    }
+}
+
+extension WatchlistCell {
+    private func configSymbolLabel() {
+        symbol.font = .preferredFont(forTextStyle: .headline)
+        symbol.adjustsFontForContentSizeCategory = true
+        symbol.text = "Symbol"
+    }
+    
+    private func configNameLabel() {
+        name.font = .preferredFont(forTextStyle: .subheadline)
+        name.adjustsFontForContentSizeCategory = true
+//        name.textColor = .secondaryLabel
+//        name.numberOfLines = 0
+        name.text = "Name"
+    }
+    
+    private func configPriceLabel() {
+        price.font = .preferredFont(forTextStyle: .footnote)
+        price.adjustsFontForContentSizeCategory = true
+        price.text = "Price"
+    }
+        
+    private func configChangeLabel() {
+        change.font = .preferredFont(forTextStyle: .footnote)
+        change.adjustsFontForContentSizeCategory = true
+        change.text = "Change"
+    }
+}
+
+extension WatchlistCell {
+    private func setupLayoutConstraints() {
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        regularConstraints = [
+            stack.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
+            stack.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor),
+            stack.bottomAnchor.constraint(equalTo: contentView.layoutMarginsGuide.bottomAnchor),
+            stack.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor),
+        ]
+    }
+    
+    private func updateLayoutConstraints() {
+        NSLayoutConstraint.activate(regularConstraints)
+    }
 }
