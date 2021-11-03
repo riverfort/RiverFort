02871f1757fd7e85c218e49bf00131bb81ff38e1
Qiuyang Nie <qyang.nie@gmail.com>
2021-11-03 09:12:41 +0000

    Remove comments

--- RiverFort/Features/Watchlist/View Controllers/WatchlistTableViewController.swift
+++ RiverFort/Features/Watchlist/View Controllers/WatchlistTableViewController.swift
@@ -29,12 +29,6 @@ class WatchlistTableViewController: UITableViewController {
         configTableView()
         configToolBar()
         configWatchlistQuoteWebSocket()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -70,15 +64,6 @@ class WatchlistTableViewController: UITableViewController {
         return cell
     }
 
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
@@ -103,17 +88,6 @@ class WatchlistTableViewController: UITableViewController {
         if isFilterOn { return false }
         return true
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 extension WatchlistTableViewController {
