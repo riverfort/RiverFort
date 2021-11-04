06c48283635633945328809aee24ad8b912a6eb0
Qiuyang Nie <qyang.nie@gmail.com>
2021-11-04 08:51:46 +0000

    Formatting

--- RiverFort/Networking/YahooFinance/YahooFinanceAPIClient.swift
+++ RiverFort/Networking/YahooFinance/YahooFinanceAPIClient.swift
@@ -20,11 +20,19 @@ struct YahooFinanceAPIClient {
     
     @available(iOS 15.0.0, *)
     static func fetchQuotes(symbols: [String]) async throws -> YahooFinanceQuoteResponse {
-        guard var quoteURLComponents = YahooFinanceAPIClient.URLs.quote else { throw FetchError.badURLComponents }
-        quoteURLComponents.queryItems = [URLQueryItem(name: "symbols", value: symbols.joined(separator: ","))]
-        guard let quoteURL = quoteURLComponents.url else { throw FetchError.badURL }
+        guard var quoteURLComponents = YahooFinanceAPIClient.URLs.quote else {
+            throw FetchError.badURLComponents
+        }
+        quoteURLComponents.queryItems = [
+            URLQueryItem(name: "symbols", value: symbols.joined(separator: ","))
+        ]
+        guard let quoteURL = quoteURLComponents.url else {
+            throw FetchError.badURL
+        }
         let (jsonData, response) = try await URLSession.shared.data(from: quoteURL)
-        guard (response as? HTTPURLResponse)?.statusCode == 200 else { throw FetchError.badResponse }
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+            throw FetchError.badResponse
+        }
         let yahooFinanceQuoteResponse = try JSONDecoder().decode(YahooFinanceQuoteResponse.self, from: jsonData)
         return yahooFinanceQuoteResponse
     }
